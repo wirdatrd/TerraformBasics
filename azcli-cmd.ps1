875dc292644eabf24e68458958603987b66b9ba9
Hugh Taylor <hugh.taylor@hotmail.com>
2021-03-23 22:49:11 -0700

    Update azcli-cmd.ps1

--- azcli-cmd.ps1
+++ azcli-cmd.ps1
@@ -2,15 +2,15 @@
 az login
 
 # if you have multiple subscriptions then select the one you want to use
-az account set -s "MPN - John Lunn"
+az account set -s "<subscription_id>"
 
 # create environment variables to initialise a shared back end (azure storage account)
-$env:RESOURCE_GROUP_NAME = 'jonnychipz-state'
-$env:STORAGE_ACCOUNT_NAME = 'jonnychipztfstate'
-$env:CONTAINER_NAME = 'tstate'
+$env:RESOURCE_GROUP_NAME = 'terraform-resources'
+$env:STORAGE_ACCOUNT_NAME = 'terraformstorage' + (Get-Random -minimum 1000 -maximum 9999).tostring()
+$env:CONTAINER_NAME = 'terraform-state-files'
 
 # Create resource group
-az group create --name $env:RESOURCE_GROUP_NAME --location uksouth
+az group create --name $env:RESOURCE_GROUP_NAME --location westus2
 
 # Create storage account
 az storage account create --resource-group $env:RESOURCE_GROUP_NAME --name $env:STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
