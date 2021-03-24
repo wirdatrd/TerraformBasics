f3decbfa838a0c79a544f150b28b44093cebac84
Hugh Taylor <hugh.taylor@hotmail.com>
2021-03-23 22:56:13 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,12 +6,13 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name   = "jonnychipz-state"
-    storage_account_name  = "jonnychipztfstate"
-    container_name        = "tstate"
+    resource_group_name   = "terraform-resources"
+    storage_account_name  = "terraformstorage2880"
+    container_name        = "terraform-state-files"
     key                   = "terraform.tfstate"
-    }
+  }
 }
+
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
@@ -19,21 +20,21 @@ provider "azurerm" {
 
 # Create a resource group
 resource "azurerm_resource_group" "myrg" {
-  name     = "jonnychipz-rg"
+  name     = "project-rg"
   location = var.primary_location
 }
 
 # Create a virtual network within the resource group
-resource "azurerm_virtual_network" "myvnet" {
-  name                = "jonnychipz-vnet"
+resource "azurerm_virtual_network" "hub_vnet" {
+  name                = "hub-vnet"
   resource_group_name = azurerm_resource_group.myrg.name
   location            = azurerm_resource_group.myrg.location
   address_space       = ["10.0.0.0/16"]
 }
 
 # Create a subnet within the vnet
-resource "azurerm_subnet" "mysubnet" {
-  name                 = "jonnychipz-subnet"
+resource "azurerm_subnet" "web_apps_subnet" {
+  name                 = "web_apps"
   resource_group_name  = azurerm_resource_group.myrg.name
   virtual_network_name = azurerm_virtual_network.myvnet.name
   address_prefixes     = ["10.0.1.0/24"]
